73365327e257075a874db531b8100c6aea9058fb
jdetwiler <jacob.detwiler@theperducogroup.com>
2023-11-01 21:13:34 -0500

    Initial Commit

---
+++ Noms/NomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomsApp.swift
+//  Noms
+//
+//  Created by Jacob Lehn Detwiler on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct NomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
